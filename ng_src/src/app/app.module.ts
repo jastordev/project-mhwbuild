27d61e6876de2a014f1901d27fa056597fb9ec2e
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2018-05-14 13:31:01 +0200

    Routing properly introduced as well as the set up of the Items feature.

--- ng_src/src/app/app.module.ts
+++ ng_src/src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
 
 import { HomeModule } from './home/home.module';
+import { ItemsModule } from './items/items.module';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ItemsComponent } from './items/items.component'; //Replace by module
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    ItemsComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     HomeModule,
+    ItemsModule,
     AppRoutingModule,
     HttpClientModule
   ],
