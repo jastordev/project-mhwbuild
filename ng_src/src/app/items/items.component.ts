e639541daefd286448c6f4c7b141267a2659b514
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2018-05-28 18:03:44 +0200

    Items now sort and display by category of dummy data.

--- ng_src/src/app/items/items.component.ts
+++ ng_src/src/app/items/items.component.ts
@@ -14,13 +14,44 @@ import { DataService } from '../shared/service/data.service';
 export class ItemsComponent implements OnInit {
 
   items : Item[];
+  categories : string[];
 
   constructor(private data : DataService) { }
 
   ngOnInit() {
+    this.categories = ["Materials", "Ammo/Coatings", "Consumables and Misc", 
+       "Decorations", "Specialized Tools"];
+
     this.data.items.subscribe( data => {
       this.items = data;
     });
+  }
+
+  itemsPerCategory(category : string) : Item[] {
+    
+    let tempList : Item[];
+
+    switch(category){
+      case "Materials":
+        tempList = this.items.filter(item => item.type == "Mat");        
+        break;
+      case "Consumables and Misc":
+        tempList = this.items.filter(item => item.type == "Misc");
+        break;
+      case "Specialized Tools":
+        tempList = this.items.filter(item => item.type == "Tool");    
+        break;
+      case "Decorations":
+        tempList =  this.items.filter(item => item.type == "Deco");
+        break;
+      case "Ammo/Coatings":
+        tempList =  this.items.filter(item => item.type == "Ammo");
+        break;     
+    }
+    
+    if(tempList.length) { return tempList };
+    return null;
+
   }
 
 }
