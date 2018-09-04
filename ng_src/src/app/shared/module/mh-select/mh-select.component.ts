7fa6b2bf014d6defba3977bba5307360108b685a
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2018-09-04 14:52:15 +0200

    [Added] Placeholder options and option styles to mh-select.

--- ng_src/src/app/shared/module/mh-select/mh-select.component.ts
+++ ng_src/src/app/shared/module/mh-select/mh-select.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector:'mh-select',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class MhSelectComponent{
 
+    @Input() options : any[];
+    selectedOpt : any;
+
     constructor(){ }
+
+    private selectOption(option : any){        
+        this.selectedOpt = option;        
+    }
 }
