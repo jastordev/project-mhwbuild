e1e7c51dfba99e60fef91661d2a3f42ff845f7c0
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2018-11-09 19:07:39 +0100

    Added some light unit tests to AppComponent.

--- ng_src/src/app/app.component.ts
+++ ng_src/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './shared/service/data.service';
 import { ModalService } from './shared/service/modal.service';
-import { ToastService } from './shared/service/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +8,7 @@ import { ToastService } from './shared/service/toast.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mhwdb';
-
-  constructor(private data: DataService, private modal : ModalService) {  }
+  constructor(private modal : ModalService) {  }
 
   removeModal(){
     this.modal.destroy();
